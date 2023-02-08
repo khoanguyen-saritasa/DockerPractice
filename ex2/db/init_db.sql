0011eea607555fc292bcae4b2a247d7b389ff545
Khoa Nguyen <khoa.nguyen@saritasa.com>
2023-02-08 14:56:40 +0700

    Add init db

---
+++ ex2/db/init_db.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+    id int,
+    email varchar(255),
+    name varchar(255),
+    password varchar(255)
+)
+
+INSERT INTO users (id, email, name, password)
+VALUES (1, 'khoanguyen@saritasa.com', 'khoanguyen', '123');
+
+INSERT INTO users (id, email, name, password)
+VALUES (2, 'test@saritasa.com','test', '123');
+
+INSERT INTO users (id, email, name, password)
+VALUES (3, 'user@saritasa.com','user2','123');
