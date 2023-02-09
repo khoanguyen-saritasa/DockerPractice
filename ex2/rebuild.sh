147d9b677151927100eb55f88f0101fb12102007
khoanguyen-saritasa <khoa.nguyen@saritasa.com>
2023-02-09 15:27:27 +0700

    Change rebuild.sh

--- ex2/rebuild.sh
+++ ex2/rebuild.sh
@@ -1,9 +1,4 @@
-# sudo docker compose down --volumes
-# sudo docker rmi -f ex2-ui ex2-api postgres
-# sudo docker compose build --no-cache
-# sudo docker compose up --renew-anon-volumes
-
 sudo docker compose down --volumes
-sudo docker rmi -f ex2-api
+sudo docker rmi -f ex2-ui ex2-api postgres
 sudo docker compose build --no-cache
 sudo docker compose up --renew-anon-volumes
